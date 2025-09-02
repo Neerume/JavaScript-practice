49ef9a3dc2c565715339bcf285d6b2c9cd698efa
Neera <neerakhatri003@gmail.com>
2025-09-02 20:37:18 +0545

    modified the functionality of reset and stop

--- StopWatch/index.js
+++ StopWatch/index.js
@@ -1,26 +1,30 @@
 const display = document.getElementById("display");
-let timer =null;
-
+let timer = null;
 let startTime=0;
-let elapsedTime=0;
+let elapsedTime = 0;
 let isRunning=false;
 
 function start(){
   if(!isRunning){
-    startTime =Date.now()-elapsedTime;
-    timer=setInterval(update,10);
+    startTime = Date.now()-elapsedTime;
+    timer = setInterval(update,10);
     isRunning=true;
   }
 }
 function stop(){
-  isRunning=false;
+  if(isRunning){
+    clearInterval(timer);
+    isRunning=false;
+  }
 }
 function reset(){
-
-
+  clearInterval(timer); //stops the time 
+  isRunning=false;
+  elapsedTime=0;
+  display.textContent="00:00:00:00";
 }
 function update(){
-  const currentTime= Date.now();
+ const currentTime= Date.now();
   elapsedTime=currentTime-startTime;
 
   let hours= Math.floor(elapsedTime/(1000*60*60)) //converting ms into hrs
